d9430e9f346c39c77bd9374847c6a22ed6ad4f7e
Dicridon <noahxiong@outlook.com>
2021-07-31 13:42:23 +0800

    memory allocator done, test to go

--- src/components/memory_manager/memory_manager.cpp
+++ src/components/memory_manager/memory_manager.cpp
@@ -2,10 +2,8 @@
 
 namespace Hill {
     namespace Memory {
-        
-        
         auto Page::allocate(size_t size, byte_ptr_t &ptr) noexcept -> void {
-            auto available = page_size - header.cursor;
+            auto available = Constants::uPAGE_SIZE - header.cursor;
             if (available < size)
                 return;
 
@@ -24,6 +22,118 @@ namespace Hill {
             auto page_address = reinterpret_cast<Page *>((reinterpret_cast<uint64_t>(ptr)) & 0xffffc000UL);
             --page_address->header.records;
             pmem_persist(this, sizeof(PageHeader));
+        }
+
+        auto Allocator::allocate(int id, size_t size, byte_ptr_t &ptr) -> void {
+            if (size > Constants::uPAGE_SIZE) {
+                    throw std::invalid_argument("Object size too large");
+                }
+
+                auto page = header.thread_busy_pages[id];
+                // on start
+                if (page)  {
+                    page->allocate(size, ptr);
+                    if (ptr != nullptr) {
+                        header.thread_busy_pages[id] = nullptr;
+                    }
+                }
+                
+                // busy page has no enough space and no thread-local free pages are available
+                if (header.thread_free_lists[id] == nullptr) {
+                    if (header.cursor + 10 >= header.base + (header.total_size / Constants::uPAGE_SIZE)) {
+                        return;
+                    } else {
+                        if (header.freelist) {
+                            // check global free list
+                            auto begin = header.freelist;
+                            auto end = header.freelist;
+                            for (int i = 0; i < 10; i++) {
+                                if (end) {
+                                    end = end->next;
+                                }
+                            }
+
+                            // on recovery, should check
+                            header.thread_free_lists[id] = begin;
+                            header.freelist = end->next;
+                            end->next = nullptr;
+                        } else {
+                            // from global heap
+                            auto tmp = header.cursor;
+                            for (int i = 0; i < 10; i++) {
+                                // dependent read/write;
+                                tmp->next = tmp + 1;
+                                tmp = tmp->next;
+                            }
+                            tmp->next = nullptr;
+                            Util::mfence();
+                            // on recovery, should check if any thread_free_list
+                            // matches cursor, if so, cursor should be incremented
+                            header.thread_free_lists[id] =  header.cursor;
+                            Util::mfence();
+                            header.cursor += 10;
+                        }
+                    }
+                }
+
+                // on recovery
+                header.thread_busy_pages[id] = header.thread_free_lists[id];
+                header.thread_free_lists[id] = header.thread_free_lists[id]->next;
+                Util::mfence();                
+                header.thread_busy_pages[id]->next = nullptr;
+                Util::mfence();
+                
+                header.thread_busy_pages[id]->allocate(size, ptr);
+        }
+
+        auto Allocator::register_thread() noexcept -> std::optional<int> {
+            for (int i = 0; i < Constants::iTHREAD_LIST_NUM; i++) {
+                if (header.thread_free_lists[i] == Constants::pTHREAD_LIST_AVAILABLE ||
+                    header.thread_busy_pages[i] == nullptr) {
+                    return i;
+                }
+            }
+            return {};
+        }
+        
+        auto Allocator::unregister_thread(int id) noexcept -> void {
+            if (id < 0 || id > Constants::iTHREAD_LIST_NUM) {
+                return;
+            }
+
+            // on recovery, should check if any thread_pending_list matches thread_busy_page
+            // if so, free list should be AVAILABLE
+            header.thread_pending_lists[id] = header.thread_busy_pages[id];
+            header.thread_busy_pages[id] = const_cast<Page *>(Constants::pTHREAD_LIST_AVAILABLE);
+        }
+        
+        auto Allocator::free(int id, byte_ptr_t &ptr) -> void {
+            auto page = reinterpret_cast<Page *>(reinterpret_cast<uint64_t>(ptr) & Constants::uPAGE_MASK);
+
+            // on recovery, should check
+            header.to_be_freed[id] = page;
+            Util::mfence();
+            if (--page->header.records == 0) {
+                // dependent read/write
+                page->next = header.thread_free_lists[id];
+                header.thread_free_lists[id] = page;
+            }
+
+            header.to_be_freed[id] = nullptr;
+        }
+
+        auto Allocator::recover() -> Enums::AllocatorRecoveryStatus {
+            if (header.magic != Constants::iALLOCATOR_MAGIC) {
+                return Enums::AllocatorRecoveryStatus::NoAllocator;
+            }
+
+            recover_pending_list();
+            recover_global_heap();
+            recover_free_lists();
+            recover_pending_list();
+            recover_to_be_freed();
+            
+            return Enums::AllocatorRecoveryStatus::Ok;
         }
     }
 }
--- src/components/memory_manager/memory_manager.hpp
+++ src/components/memory_manager/memory_manager.hpp
@@ -11,11 +11,28 @@ namespace Hill {
     // Specialized memory manager for KV
     // For durability, use this with a WAL
     namespace Memory {
-        inline void mfence(void) {
-            asm volatile("mfence":::"memory");
+        struct Page;
+        namespace Constants {
+            static constexpr size_t uPAGE_SIZE = 16 * 1024;
+            static constexpr uint64_t uPAGE_MASK = 0xffffc000UL;
+            static constexpr Page * pTHREAD_LIST_AVAILABLE = nullptr;
+            static constexpr int iTHREAD_LIST_NUM = 64;            
+            static constexpr uint64_t iALLOCATOR_MAGIC = 0xabcddcbaUL;
         }
 
-        
+        namespace Enums {
+            enum class AllocatorRecoveryStatus {
+                Ok,
+                Corrupted,
+                NoAllocator,
+            };
+        }
+
+        namespace Util {
+            inline void mfence(void) {
+                asm volatile("mfence":::"memory");
+            }
+        }
         /*
          * A Page(16KB) is the basic memory alloction granularity, more 
          * fine grained allocation is performed within each page in each
@@ -72,7 +89,7 @@ namespace Hill {
             static auto make_page(const byte_ptr_t &in) -> Page & {
                 auto page_ptr = reinterpret_cast<Page *>(in);
                 page_ptr->header.records = 0;
-                page_ptr->header.cursor = 8; // offsetting the header;
+                page_ptr->header.cursor = sizeof(PageHeader); // offsetting the header;
                 page_ptr->next = 0;
                 void(page_ptr->_content); // silent the warnings
                 return *page_ptr;
@@ -92,35 +109,17 @@ namespace Hill {
 #endif
             }
 
-            static constexpr size_t page_size = 16 * 1024;
-
             PageHeader header;
-            uint64_t _content[16 * 1024 - 16];
+            uint64_t _content[Constants::uPAGE_SIZE - sizeof(PageHeader)];
             Page *next;
         };
 
-        namespace Constants {
-            static const Page * pTHREAD_LIST_AVAILABLE = nullptr;
-            static const int iTHREAD_LIST_NUM = 64;            
-            static const uint64_t iALLOCATOR_MAGIC = 0xabcddcbaUL;
-
-        }
-
-        namespace Enums {
-            enum class AllocatorRecoveryStatus {
-                Ok,
-                Corrupted,
-                NoAllocator,
-            };
-        }
 
         /*
          * Given a continuous memory region, this calss manages it at 16KB granularity
-         * 
-         * The allocation starts at the first 16KB-aligned page and any space ahead this
-         * page is reserved for metadata. If the given region is already 16KB aligned, 
-         * the first page is reserved for metadata.
-         * 
+         *
+         * The memory region is 16KB aligned and the first page is always reserved
+         * for metadata
          */
         class Allocator {
         private:
@@ -129,13 +128,18 @@ namespace Hill {
                 uint64_t magic;
                 size_t total_size;
                 Page *freelist;              // only for page reuse
+                Page *base;
                 Page *cursor;
                 Page *thread_free_lists[Constants::iTHREAD_LIST_NUM]; // avoid memory leaks
 
                 // This list is purely for the convenience of unregisteration
                 // Free pages in each thread's free list is moved here upon
-                // unregisteration. If a new thread is to register, the 
+                // unregisteration. If a new thread is to register, the move
+                // it back
                 Page *thread_pending_lists[Constants::iTHREAD_LIST_NUM];
+
+                // This list is used to prevent page leak during a free
+                Page *to_be_freed[Constants::iTHREAD_LIST_NUM];
                 Page *thread_busy_pages[Constants::iTHREAD_LIST_NUM];
             };
         public:
@@ -146,7 +150,7 @@ namespace Hill {
             auto operator=(const Allocator &) -> Allocator & = delete;
             auto operator=(Allocator &&) -> Allocator & = delete;
 
-            static auto make_allocator(const byte_ptr_t &base, size_t size) -> Allocator * {
+            static auto make_allocator(byte_ptr_t &base, size_t size) -> Allocator * {
                 auto allocator = reinterpret_cast<Allocator *>(base);
                 switch(allocator->recover()){
                 case Enums::AllocatorRecoveryStatus::Ok:
@@ -162,47 +166,86 @@ namespace Hill {
                 allocator->header.magic = Constants::iALLOCATOR_MAGIC;
                 allocator->header.total_size = size;
                 allocator->header.freelist = nullptr;
-                allocator->header.cursor = reinterpret_cast<Page *>(base + sizeof(AllocatorHeader));
+
+                auto aligned = reinterpret_cast<Page *>(reinterpret_cast<uint64_t>(base + sizeof(AllocatorHeader)) & Constants::uPAGE_MASK);
+                allocator->header.base = reinterpret_cast<Page *>(aligned + 1);
+                allocator->header.cursor = allocator->header.base;
 
                 for (int i = 0; i < Constants::iTHREAD_LIST_NUM; i++) {
                     allocator->header.thread_free_lists[i] = const_cast<Page *>(Constants::pTHREAD_LIST_AVAILABLE);
                     allocator->header.thread_pending_lists[i] = const_cast<Page *>(Constants::pTHREAD_LIST_AVAILABLE);
                     allocator->header.thread_busy_pages[i] = nullptr;
+                    allocator->header.to_be_freed[i] = nullptr;
                 }
                 return allocator;
             }
 
-            auto register_thread() noexcept -> std::optional<int> {
-                for (int i = 0; i < Constants::iTHREAD_LIST_NUM; i++) {
-                    if (header.thread_free_lists[i] == Constants::pTHREAD_LIST_AVAILABLE) {
-                        return i;
-                    }
-                }
-                return {};
-            }
+            auto register_thread() noexcept -> std::optional<int>;
+            auto unregister_thread(int id) noexcept -> void;
+            auto allocate(int id, size_t size, byte_ptr_t &ptr) -> void;
+            
+            auto free(int id, byte_ptr_t &ptr) -> void;
 
-            auto unregister_thread(int id) noexcept -> void {
-                if (id < 0 || id > Constants::iTHREAD_LIST_NUM) {
-                    return;
-                }
-
-                header.thread_pending_lists[id] = header.thread_busy_pages[id];
-                header.thread_busy_pages[id] = const_cast<Page *>(Constants::pTHREAD_LIST_AVAILABLE);
-            }
-
-            auto allocate(size_t size, byte_ptr_t &ptr) -> byte_ptr_t {
-                
-            }
-
-            auto free(byte_ptr_t &ptr) -> void {
-
-            }
-
-            auto recover() -> Enums::AllocatorRecoveryStatus {
-                
-            }
+            auto recover() -> Enums::AllocatorRecoveryStatus;
         private:
             AllocatorHeader header;
+            
+            auto recover_global_free_list() -> void {
+                for (int i = 0; i < Constants::iTHREAD_LIST_NUM; i++) {
+                    // on-going allocation is detected
+                    if (header.thread_free_lists[i] == header.freelist) {
+                        auto end = header.freelist;
+                        for (int i = 0; i < 10; i++) {
+                            if (end) {
+                                end = end->next;
+                            }
+                        }
+
+                        header.freelist = end->next;
+                        end->next = nullptr;
+                    }
+                }
+            }
+
+            auto recover_free_lists() -> void {
+                for (int i = 0; i < Constants::iTHREAD_LIST_NUM; i++) {
+                    // on-going allocation
+                    if (header.thread_busy_pages[i] == header.thread_free_lists[i]) {
+                        header.thread_free_lists[i] = header.thread_free_lists[i]->next;
+                        header.thread_busy_pages[i]->next = nullptr;
+                    }
+                }
+            }
+            
+            auto recover_global_heap() -> void {
+                for (int i = 0; i < Constants::iTHREAD_LIST_NUM; i++) {
+                    if (header.thread_free_lists[i] == header.cursor) {
+                        header.cursor += 10;
+                    }
+                }
+            }
+            
+            auto recover_pending_list() -> void {
+                // on-going unregisteration
+                for (int i = 0; i < Constants::iTHREAD_LIST_NUM; i++) {
+                    if (header.thread_pending_lists[i] == header.thread_busy_pages[i]) {
+                        header.thread_busy_pages[i]->next = header.thread_free_lists[i];
+                        header.thread_free_lists[i] = header.thread_busy_pages[i];
+                        header.thread_busy_pages[i] = nullptr;
+                    }
+                }
+            }
+
+            auto recover_to_be_freed() -> void {
+                for (int i = 0; i < Constants::iTHREAD_LIST_NUM; i++) {
+                    if (header.to_be_freed[i]->next != nullptr) {
+                        // freelists may have changed during recovery
+                        header.to_be_freed[i]->next = header.thread_free_lists[i];
+                        header.thread_free_lists[i] = header.to_be_freed[i];
+                        header.to_be_freed[i] = nullptr;
+                    }
+                }
+            }
         };
     }
 }
