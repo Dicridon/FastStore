04ed09c3f6fb0083f9565c74c073458193bb947c
Dicridon <noahxiong@outlook.com>
2021-10-19 15:07:57 +0800

    build flags corrected

--- build_erpc.rb
+++ build_erpc.rb
@@ -1,8 +1,6 @@
 # building script for eRPC on infiniband machines
 Dir.chdir('./third-party/eRPC') do
-  return unless system 'cmake . -DTRANSPORT=infiniband -DROCE=on -DPERF=on -DAZURE=off'
+  return unless system 'cmake . -DTRANSPORT=infiniband -DROCE=on -DPERF=on -DCONFIG_IS_AZURE=off'
 
   return unless system 'make -j'
-
-  return unless system 'cp ./src/config.h ../src'
 end
