f6f1e4db28fcc3ff4b1bdbe2222e441b2a37aa42
Dicridon <noahxiong@outlook.com>
2021-10-19 15:12:25 +0800

    eRPC is really wierd

--- build_erpc.rb
+++ build_erpc.rb
@@ -1,6 +1,6 @@
 # building script for eRPC on infiniband machines
 Dir.chdir('./third-party/eRPC') do
-  return unless system 'cmake . -DTRANSPORT=infiniband -DROCE=on -DPERF=on -DCONFIG_IS_AZURE=off'
+  return unless system 'cmake . -DTRANSPORT=infiniband -DROCE=on -DPERF=on -DCONFIG_IS_AZURE=0'
 
   return unless system 'make -j'
 end
