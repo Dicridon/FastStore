8b7046723a4912bb2f09e59233aa8d58b68e9f83
Dicridon <noahxiong@outlook.com>
2021-10-29 15:58:09 +0800

    using 64bit

--- tests/test_erpc.cpp
+++ tests/test_erpc.cpp
@@ -17,11 +17,11 @@ auto server_handler(erpc::ReqHandle *req_handle, void *context) -> void {
     auto server_ctx = reinterpret_cast<ServerContext *>(context);
     auto requests = req_handle->get_req_msgbuf();
 
-    auto in_data = *reinterpret_cast<uint8_t *>(requests->buf);
-    std::cout << "got " << in_data << " from client\n";:
+    auto in_data = *reinterpret_cast<uint64_t *>(requests->buf);
+    std::cout << "got " << in_data << " from client\n";
     auto &resp = req_handle->dyn_resp_msgbuf;
-    resp = server_ctx->rpc->alloc_msg_buffer_or_die(sizeof(uint8_t));
-    *reinterpret_cast<uint8_t *>(resp.buf) = in_data + 1;
+    resp = server_ctx->rpc->alloc_msg_buffer_or_die(sizeof(uint64_t));
+    *reinterpret_cast<uint64_t *>(resp.buf) = in_data + 1;
     server_ctx->rpc->enqueue_response(req_handle, &resp);
 };
 
@@ -40,7 +40,7 @@ auto server() -> void {
 auto res_cont(void *context, [[maybe_unused]]void *tag) {
     auto ctx = reinterpret_cast<ClientContext *>(context);
     auto &resp = ctx->resp_buf;
-    std::cout << "got " << *reinterpret_cast<uint8_t *>(resp.buf) << " from server\n";
+    std::cout << "got " << *reinterpret_cast<uint64_t *>(resp.buf) << " from server\n";
  };
 
 
@@ -49,8 +49,8 @@ auto client() -> void {
     ClientContext ctx;
     ctx.rpc = new erpc::Rpc<erpc::CTransport>(nexus, reinterpret_cast<void *>(&ctx),
                                               0, RPCWrapper::ghost_sm_handler);
-    ctx.req_buf = ctx.rpc->alloc_msg_buffer_or_die(sizeof(uint8_t));
-    ctx.resp_buf = ctx.rpc->alloc_msg_buffer_or_die(sizeof(uint8_t));
+    ctx.req_buf = ctx.rpc->alloc_msg_buffer_or_die(sizeof(uint64_t));
+    ctx.resp_buf = ctx.rpc->alloc_msg_buffer_or_die(sizeof(uint64_t));
     auto session = ctx.rpc->create_session("127.0.0.1:31850", 0);
     if (session < 0) {
         std::cout << "can't create session\n";
@@ -58,10 +58,10 @@ auto client() -> void {
     }
 
 
-    uint8_t counter = 0;
+    uint64_t counter = 0;
     while(true) {
         std::cout << "sending " << counter << " to server\n";
-        *reinterpret_cast<uint8_t *>(ctx.req_buf.buf) = counter++;
+        *reinterpret_cast<uint64_t *>(ctx.req_buf.buf) = counter++;
         ctx.rpc->enqueue_request(session, 0, &ctx.req_buf, &ctx.resp_buf, res_cont, nullptr);
         sleep(1);
         ctx.rpc->run_event_loop_once();
