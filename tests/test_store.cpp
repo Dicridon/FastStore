4ef38f89ed3bf13e38669a33cfe351fbeb72c8db
Dicridon <noahxiong@outlook.com>
2021-11-25 21:39:26 +0800

    false sampling

--- tests/test_store.cpp
+++ tests/test_store.cpp
@@ -88,7 +88,7 @@ auto main(int argc, char *argv[]) -> int {
                       << "average latency: " << stats[i].latencies.insert_avg_latency() << "us, "
                       << "p90: " << stats[i].latencies.insert_p90_latency() << "us, "
                       << "p99: " << stats[i].latencies.insert_p99_latency() << "us, "
-                      << "p999: " << stats[i].latencies.insert_p999_latency() << "us, "
+                      << "p999: " << stats[i].latencies.insert_p999_latency() << "us"
                       << "\n";
         }
 
@@ -100,11 +100,11 @@ auto main(int argc, char *argv[]) -> int {
         std::cout << ">> Reporting searches: \n";
         for (int i = 0; i < threads; i++) {
             std::cout << "[[ Thread " << i << "]]:\n";
-            std::cout << "-->> throughput: " << stats[i].throughputs.insert_throughput() << " Ops/second"
-                      << "-->> average latency: " << stats[i].latencies.insert_avg_latency() << "us, "
-                      << "-->> p90: " << stats[i].latencies.insert_p90_latency() << "us, "
-                      << "-->> p99: " << stats[i].latencies.insert_p99_latency() << "us, "
-                      << "-->> p999: " << stats[i].latencies.insert_p999_latency() << "us, "
+            std::cout << "throughput: " << stats[i].throughputs.search_throughput() << " Ops/second"
+                      << "average latency: " << stats[i].latencies.search_avg_latency() << "us, "
+                      << "p90: " << stats[i].latencies.search_p90_latency() << "us, "
+                      << "p99: " << stats[i].latencies.search_p99_latency() << "us, "
+                      << "p999: " << stats[i].latencies.search_p999_latency() << "us"
                       << "\n";
         }
     }
