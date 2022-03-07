ce3091755559157dce061a52588c91e16ec397cb
Dicridon <noahxiong@outlook.com>
2022-03-07 20:39:59 +0800

    empty message test

--- src/components/store/store.cpp
+++ src/components/store/store.cpp
@@ -200,10 +200,12 @@ namespace Hill {
 
                 while(true) {
                     s_ctx.rpc->run_event_loop(2000);
+#ifdef __HILL_INFO__
                     std::cout << ">> Insert breakdown: "; s_ctx.handle_sampler->report_insert(); std::cout << "\n";
                     std::cout << ">> Search breakdown: "; s_ctx.handle_sampler->report_search(); std::cout << "\n";
                     std::cout << ">> Update breakdown: "; s_ctx.handle_sampler->report_update(); std::cout << "\n";
                     std::cout << ">> Range breakdown: "; s_ctx.handle_sampler->report_scan(); std::cout << "\n\n";
+#endif
                 }
 
                 this->server->unregister_thread(tid);
@@ -321,6 +323,7 @@ namespace Hill {
 
         auto StoreServer::insert_handler(erpc::ReqHandle *req_handle, void *context) -> void {
             auto ctx = reinterpret_cast<ServerContext *>(context);
+            /*                        
             auto server = ctx->server;
             auto handle_sampler = ctx->handle_sampler;
             auto &sampler = handle_sampler->insert_sampler;
@@ -386,9 +389,11 @@ namespace Hill {
 #ifdef __HILL_SAMPLE__
             }
 #endif
+            */
             auto &resp = req_handle->pre_resp_msgbuf;
             constexpr auto total_msg_size = sizeof(Enums::RPCOperations) + sizeof(Enums::RPCStatus) + sizeof(Memory::PolymorphicPointer);
             ctx->rpc->resize_msg_buffer(&resp, total_msg_size);
+            /*
 #ifdef __HILL_SAMPLE__
             {
                 SampleRecorder<uint64_t> _(sampler, HandleSampler::RESP_MSG);
@@ -425,13 +430,14 @@ namespace Hill {
             {
                 SampleRecorder<uint64_t> _(sampler, HandleSampler::RESP);
 #endif
-                ctx->rpc->enqueue_response(req_handle, &resp);
+                */
+            ctx->rpc->enqueue_response(req_handle, &resp);
 #ifdef __HILL_SAMPLE__
             }
 #endif
-            if (msg.output.status.load() == Indexing::Enums::OpStatus::Failed) {
-                std::cout << "Inserting " << key->to_string() << " failed\n";
-            }
+        // if (msg.output.status.load() == Indexing::Enums::OpStatus::Failed) {
+        // std::cout << "Inserting " << key->to_string() << " failed\n";
+        // }
         }
 
         auto StoreServer::update_handler(erpc::ReqHandle *req_handle, void *context) -> void {
@@ -1042,7 +1048,7 @@ namespace Hill {
                     break;
                 }
                 ctx->is_done = true;
-#ifdef __HILL_SAMPLE__                
+#ifdef __HILL_SAMPLE__
             }
 #endif
         }
