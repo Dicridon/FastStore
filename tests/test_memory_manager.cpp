d8f694e5736e50b5c298591df635f81af840a9b6
Dicridon <noahxiong@outlook.com>
2022-05-06 20:44:17 +0800

    test allocator performance

--- tests/test_memory_manager.cpp
+++ tests/test_memory_manager.cpp
@@ -1,15 +1,30 @@
 #include "memory_manager/memory_manager.hpp"
 
 #include <iostream>
+#include <chrono>
+#include <vector>
+
+#include <libpmem.h>
 
 using namespace Hill::Memory;
 
 int main() {
-    constexpr size_t total = 3 * Constants::uPAGE_SIZE;
+    size_t total = 1024 * 1024 * 1024;
+    size_t mapped_size = 0;
+    auto base = reinterpret_cast<byte_ptr_t>(pmem_map_file("/mnt/pmem2/pm",
+                                                           total,
+                                                           PMEM_FILE_CREATE, 0666,
+                                                           &mapped_size, nullptr));
+    if (base == nullptr) {
+        std::cout << ">> Unable to map pmem file\n";
+        std::cout << ">> Errno is " << errno << ": " << strerror(errno) << "\n";
+        return -1;
+    } else {
+        std::cout << ">> " << mapped_size / 1024 / 1024 / 1024.0 << "GB pmem is mapped at "
+                  << reinterpret_cast<void *>(base) << "\n";
+    }
     
-    auto space = new byte_t[total];
-
-    auto allocator = Allocator::make_allocator(space, total);
+    auto allocator = Allocator::make_allocator(base, total);
     if (!allocator) {
         std::cout << "Allocation created failed\n";
         return -1;
@@ -23,12 +38,26 @@ int main() {
 
     auto tid = _id.value();
     byte_ptr_t ptr = nullptr;
-    for (int i = 0; i < 10; i++) {
-        allocator->allocate(tid, 34, ptr);
-        if (i == 0)
-            *(uint64_t *)ptr = 1234554321UL + i;
-        allocator->free(tid, ptr);
-    }
 
-    std::cout << *(uint64_t *)ptr << "\n";
+    std::vector<size_t> chunks {32, 64, 128, 512, 1024};
+    std::chrono::time_point<std::chrono::steady_clock> start, end;
+    for (auto &c : chunks) {
+        start = std::chrono::steady_clock::now();
+        for (int i = 0; i < 1000; i++) {
+            allocator->allocate(tid, c, ptr);
+        }
+
+        end = std::chrono::steady_clock::now();
+        std::cout << "HA takes " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "\n";
+
+        start = std::chrono::steady_clock::now();
+        for (int i = 0; i < 1000; i++) {
+            ptr = new byte_t[c];
+        }
+        end = std::chrono::steady_clock::now();
+        std::cout << "Malloc takes " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "\n";
+
+        
+        
+    }
 }
